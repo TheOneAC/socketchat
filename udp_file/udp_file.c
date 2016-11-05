09afb2825f2ae8931b6cdcdda28546e82c909116
The One Ac <scuhss@gmail.com>
2016-11-05 22:26:03 +0800

    file trans ok but not pdf and so on

--- udp_file/udp_file.c
+++ udp_file/udp_file.c
@@ -1,5 +1,5 @@
 #include "udp_file.h"
-
+char *SERVER_ADDR = "127.0.0.1";
 void err_msg(const char *msg){
 	perror(msg);
 	exit(1);
@@ -17,9 +17,40 @@ void clit_addr_init(sockaddr_in *clit_addr){
 	clit_addr->sin_port = htons(0);
 }
 void clit_serv_addr_init(sockaddr_in * clit_serv_addr,const char* ipaddr){
-	bzero(clit_serv_sock,sizeof(*clit_serv_addr));
+	bzero(clit_serv_addr,sizeof(*clit_serv_addr));
 	clit_serv_addr->sin_family = AF_INET;
 	clit_serv_addr->sin_port = htons(SERVER_PORT);
-	if(inet_aton(ipaddr, clit_serv_addr) == 0)
-		err_msg("unavailable server ip ");
+	if(ipaddr != NULL)inet_aton(ipaddr, &clit_serv_addr->sin_addr);
+	else inet_aton(SERVER_ADDR, &clit_serv_addr->sin_addr);
+	/*if(inet_aton(SERVER_ADDR, &clit_serv_addr->sin_addr) == 0)
+		err_msg("unavailable server ip ");*/
 }
+void send_filename(char *filename,int clit_sock,sockaddr_in *clit_serv_addr){
+	bzero(filename,sizeof(*filename));
+	puts("please input the filenme you want :");
+	fgets(filename,MAXLINE,stdin);
+	sendto(clit_sock,filename,sizeof(filename),0,(struct sockaddr*)clit_serv_addr,sizeof(*clit_serv_addr));
+}
+
+
+void Gen_md5(const char* filename, char* md5_buffer)
+{  
+	FILE *fp = fopen(filename,"r+");
+    MD5_CTX ctx = { 0 };  
+    int len = 0;  
+    unsigned char buffer[1024] = {0};  
+    unsigned char digest[16] = {0};  
+    int SIZE;  
+    MD5_Init (&ctx);  
+    while ((len = fread (buffer, 1, MAXLINE, fp)) > 0)  
+    {  
+        MD5_Update (&ctx, buffer, len);  
+    }    
+    MD5_Final (digest, &ctx);
+    char tmp1[3] = {0};  
+    for(int i = 0; i < 16; i++ )  
+    {  
+        sprintf(tmp1,"%02X", digest[i]);   
+        strcat(md5_buffer, tmp1);   
+    }  
+} 
--- udp_file/udp_file.h
+++ udp_file/udp_file.h
@@ -4,8 +4,14 @@
 #include <string.h>
 #include <sys/types.h>
 #include <netinet/in.h>  
+#include <arpa/inet.h>
+#include <time.h>
 #include <openssl/md5.h>
-#define MAXLINE 1000
+#include <sys/stat.h> 
+ #include <fcntl.h>
+
+
+#define MAXLINE 1024
 #define SERVER_PORT 6789
 
 typedef struct sockaddr_in sockaddr_in;
@@ -14,3 +20,5 @@ void err_msg(const char *msg);
 void serv_addr_init(sockaddr_in * serv_sock);
 void clit_addr_init(sockaddr_in *clit_sock);
 void clit_serv_addr_init(sockaddr_in * clit_serv_sock,const char *ipaddr);
+void send_filename(char *filename,int clit_sock,sockaddr_in *clit_serv_addr);
+void Gen_md5(const char* filename, char* md5_buffer);
